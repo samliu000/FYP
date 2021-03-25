8408feb619ff44a786b88256f04b27fcea79dc8c
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-03-24 21:33:12 -0700

    Initial commit

---
+++ app/src/main/java/com/lahacks/fyp/MainActivity.java
@@ -0,0 +1,143 @@
+package com.lahacks.fyp;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
+
+import static java.security.AccessController.getContext;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int RESULT_ENABLE = 11;
+    public static final String TAG = "MainActivity";
+    DevicePolicyManager dpm;
+    DeviceAdminReceiver dar;
+    Button killButton;
+    Button unkillButton;
+    Button mysteryButton;
+    private ComponentName compName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize device policy manager
+        dpm = (DevicePolicyManager) this.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        dar = new DeviceAdminReceiver();
+        compName = new ComponentName(this, MyAdmin.class);
+
+        // initialize buttons
+        killButton = findViewById(R.id.killButton);
+        unkillButton = findViewById(R.id.unkillButton);
+        mysteryButton = findViewById(R.id.mysteryButton);
+
+        // print list of apps
+        getAllApplications();
+
+        // apps to ban/unban
+        String[] listOfPackages = {"com.google.android.youtube", "com.zhiliaoapp.musically"};
+
+        // trap
+        mysteryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You fool! It does nothing", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // logic for the killButton
+        killButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                boolean active = dpm.isAdminActive(compName);
+
+                // kill tik tok!
+                if (active) {
+                    try {
+                        // suspend package
+                        dpm.setPackagesSuspended(compName, listOfPackages, true);
+                        Toast.makeText(MainActivity.this, "Apps Successfully Blocked", Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Log.d(TAG, "Security Exception " + e);
+                    }
+                }
+
+                // ask for admin privilege
+                else {
+
+                    // create intent to go to page where you ask for admin permission
+                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "We need " +
+                            "this permission to block apps");
+                    startActivityForResult(intent, RESULT_ENABLE);
+                }
+            }
+        });
+
+        unkillButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                boolean active = dpm.isAdminActive(compName);
+
+                // unkill tik tok!
+                if (active) {
+                    try {
+                        // suspend package
+                        dpm.setPackagesSuspended(compName, listOfPackages, false);
+                        Toast.makeText(MainActivity.this, "Apps Successfully Unblocked", Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Log.d(TAG, "Security Exception " + e);
+                    }
+                }
+
+                // ask for admin privilege
+                else {
+
+                    // create intent to go to page where you ask for admin permission
+                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "We need " +
+                            "this permission to block apps");
+                    startActivityForResult(intent, RESULT_ENABLE);
+                }
+            }
+        });
+    }
+
+    /**
+     * Get all the applications on the phone
+     * @return
+     */
+    public List<ApplicationInfo> getAllApplications() {
+        final PackageManager pm = getPackageManager();
+        //get a list of installed apps.
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        // print out relevant info about each application
+        for (ApplicationInfo packageInfo : packages) {
+            Log.d(TAG, "Installed package :" + packageInfo.packageName);
+            Log.d(TAG, "Source dir : " + packageInfo.sourceDir);
+            Log.d(TAG, "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+        }
+
+        return packages;
+    }
+}
---
+++ app/src/main/java/com/lahacks/fyp/MyAdmin.java
@@ -0,0 +1,22 @@
+package com.lahacks.fyp;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+public class MyAdmin extends DeviceAdminReceiver {
+    @Override
+    public void onEnabled(@NonNull Context context, @NonNull Intent intent) {
+        super.onEnabled(context, intent);
+        Toast.makeText(context, "Device Admin : enabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisabled(@NonNull Context context, @NonNull Intent intent) {
+        super.onEnabled(context, intent);
+        Toast.makeText(context, "Device Admin : disabled", Toast.LENGTH_SHORT).show();
+    }
+}
