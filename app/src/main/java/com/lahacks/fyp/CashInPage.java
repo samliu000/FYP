d063f5a764487a39cece93e02d35c558c1391474
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-03-24 21:44:57 -0700

    Created new screens for Pia and Cam

---
+++ app/src/main/java/com/lahacks/fyp/CashInPage.java
@@ -0,0 +1,14 @@
+package com.lahacks.fyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CashInPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cash_in_page);
+    }
+}
